1d14554aa3e51233cc0fa8046aa521733fc38a7a
elcabezzonn <matiasdavaro@gmail.com>
2017-05-16 10:51:36 -0400

    Add files via upload

---
+++ bro_dump.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#this is a script to do protocol state machine with bro. I got this information from Liam Randall when he was instructing a class on bro at 
+#bsides. It has helped me when looking for fields in bro when you are trying to script things 
+
+#this allows you to pass I think unlimited commands to the script
+filter=$@
+
+usage() {
+  echo "usage: $0"
+  echo "provide a bpf filter argument to the script so it can dump it into a dump.log for protocol state machine"
+  exit 1
+}
+
+if [[ $# -lt 1 ]]; then
+  usage 
+fi
+
+#switch -e allows for tab completion 
+read -ep "pcap plz : " pcap
+bro -C -r "$pcap" policy/misc/dump-events.bro  "PacketFilter::default_capture_filter = \"""$filter""\"" local >> dump.log
