bb6b693caeec4373a5af4d8d106789fe006ad739
elcabezzonn <matiasdavaro@gmail.com>
2017-05-16 02:01:30 -0400

    Update download_pcap.sh

--- download_pcap.sh
+++ download_pcap.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#usually use this in combination with another_pcap_dir.sh. I use this typically to grab pcaps from the awesome malwaretraffic
+#analysis, broadanalysis and all those other great sites that share their pcaps to the community. 
 
 wget $1 && unzip -P infected ${1##*/}
 
