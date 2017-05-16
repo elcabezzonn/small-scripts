cf8c312800e5c1e437a32edec360dffd557dea88
elcabezzonn <matiasdavaro@gmail.com>
2017-05-16 01:37:38 -0400

    Update another_pcaps_to_dir.sh

--- another_pcaps_to_dir.sh
+++ another_pcaps_to_dir.sh
@@ -1,2 +1,2 @@
-find . -maxdepth 1 -type f -regex '.*\.pcap\(ng\)?' -print0 | xargs -0 -l /bin/bash -c 'mkdir "${1%.*}"; /bin/bash /home/elcabezon/bin/./shatherlogs.sh "$1"  && mv "$1" *log extract_files "${1%.*}"' /bin/bash
+find . -maxdepth 1 -type f -regex '.*\.pcap\(ng\)?' -print0 | xargs -0 -l /bin/bash -c 'mkdir "${1%.*}"; /bin/bash /home/elcabezon/bin/./gatherlogs.sh "$1"  && mv "$1" *log extract_files "${1%.*}"' /bin/bash
 
